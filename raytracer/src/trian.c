3d71557577f0ec0a0b0805304071416458f52a47
Victor "multun" Collod <victor.collod@epita.fr>
2017-10-28 22:02:31 +0200

    trian: valid intersection

--- raytracer/src/trian.c
+++ raytracer/src/trian.c
@@ -11,27 +11,19 @@ static inline s_vect trian_normal(const s_trian *trian)
 }
 
 
-static inline bool is_in_triangle(const s_trian *tri, const s_vect *pt)
+bool same_side(s_vect p1, s_vect p2, s_vect a, s_vect b)
 {
-  // https://math.stackexchange.com/questions/4322/
-  const s_vect a = tri->vertices[0].v;
-  const s_vect b = tri->vertices[1].v;
-  const s_vect c = tri->vertices[2].v;
+  s_vect cp1 = vect_cross(vect_sub(b, a), vect_sub(p1, a));
+  s_vect cp2 = vect_cross(vect_sub(b, a), vect_sub(p2, a));
+  return vect_dot(cp1, cp2) >= 0;
+}
 
-  flt ab = vect_dist(a, b);
-  flt ac = vect_dist(a, c);
-  flt pa = vect_dist(*pt, a);
-  flt pb = vect_dist(*pt, b);
-  flt pc = vect_dist(*pt, c);
 
-  // two times the area of the triangle
-  flt area2 = ab * ac;
-  flt alpha = pb * pc / area2;
-  flt beta = pc * pa / area2;
-  flt gamma = 1. - alpha - beta;
-#define CRIT(K) (0. <= (K) && (K) <= 1.)
-  return CRIT(alpha) && CRIT(beta) && CRIT(gamma);
-#undef CRIT
+bool point_in_triangle(s_vect p, s_vect a, s_vect b, s_vect c)
+{
+  return (same_side(p, a, b, c)
+         && same_side(p, b, a, c)
+         && same_side(p, c, a, b));
 }
 
 
@@ -42,6 +34,9 @@ flt trian_intersect(const s_trian *tri, const s_ray *ray,
   // http://mathworld.wolfram.com/NormalVector.html
   //  n.x * x + n.y * y + n.z * z + (- n.z * z0 - n.x * x0 - n.y * y0) = 0
   s_vect A = tri->vertices[0].v;
+  s_vect B = tri->vertices[1].v;
+  s_vect C = tri->vertices[2].v;
+
   flt d = - n.x * A.x - n.y * A.y - n.z * A.z;
 
   flt denom = vect_dot(n, ray->dir);
@@ -55,7 +50,7 @@ flt trian_intersect(const s_trian *tri, const s_ray *ray,
     return -INFINITY;
 
   res->orig = vect_add(ray->orig, vect_mult(ray->dir, dist));
-  if (!is_in_triangle(tri, &res->orig))
+  if (!point_in_triangle(res->orig, A, B, C))
     return -INFINITY;
 
   res->dir = vect_normalize(n);
