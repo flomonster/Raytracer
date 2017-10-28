9f237758e91996037910851fbed775b6ed29e027
Victor "multun" Collod <victor.collod@epita.fr>
2017-10-28 17:49:15 +0200

    ppm: add tooling macros

--- raytracer/includes/ppm.h
+++ raytracer/includes/ppm.h
@@ -6,6 +6,8 @@
 
 
 typedef s_color s_pix;
+#define PIX(R, G, B) COLOR(R, G, B)
+
 
 typedef struct image
 {
@@ -14,6 +16,8 @@ typedef struct image
   s_pix pix[];
 } s_image;
 
+
+#define IMG_AT(Img, X, Y) ((Img)->pix[(Img)->w * (Y) + (X)])
 
 s_image *image_alloc(size_t w, size_t h);
 void image_render(s_image *img, FILE *out);
